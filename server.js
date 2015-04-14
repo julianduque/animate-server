4efa254fda740301af96e9fdf903c5e5c30bc2e5
Julian Duque <julianduquej@gmail.com>
2015-04-14 11:26:45 -0500

    Fix: Servir index y app.js sin problemas
    En este commit creamos dos metodos `serveIndex` y `serveApp`, estos
    serán utilizados para servir nuestro archivo index y javascript, pero...
    si seguimos este patrón nuestro código empezará a ser muy dificil de
    mantener de ahora en adelante.
    
    En los siguientes pasos vamos a ver como podemos agregar es lio sin
    necesidad de utilizar un framework completo, para esto usaremos
    librerias que solucionan problemas especificos como el de servir
    archivos estaticos y manejar rutas

--- server.js
+++ server.js
@@ -13,10 +13,35 @@ server.on('listening', onListening)
 server.listen(port)
 
 function onRequest (req, res) {
+  let uri = req.url
+
+  if (uri.startsWith('/index') || uri === '/') return serveIndex(res)
+
+  if (uri.startsWith('/app.js')) return serveApp(res)
+
+  res.statusCode = 404
+  res.setHeader('Content-Type', 'text/plain')
+  res.end(`404 Not found: ${uri}`)
+}
+
+function serveIndex (res) {
   let index = path.join(__dirname, 'public', 'index.html')
   let rs = fs.createReadStream(index)
 
   res.setHeader('Content-Type', 'text/html')
+  rs.pipe(res)
+
+  rs.on('error', function (err) {
+    res.setHeader('Content-Type', 'text/plain')
+    res.end(err.message)
+  })
+}
+
+function serveApp (res) {
+  let app = path.join(__dirname, 'public', 'app.js')
+  let rs = fs.createReadStream(app)
+
+  res.setHeader('Content-Type', 'text/javascript')
   rs.pipe(res)
 
   rs.on('error', function (err) {
