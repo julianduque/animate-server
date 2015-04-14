b0277af75a93619466e2b7f75b3e88b565a190cf
Julian Duque <julianduquej@gmail.com>
2015-04-14 10:18:46 -0500

    Agregar callback en el request handle y en el listen
    Aquí agregamos un par de callbacks a nuestro servidor, el primero es una
    función anónima que se ejecutará cada vez que llegue una petición http a
    nuestro servidor, el segundo se ejecutará cuando el servidor inicie por
    primer vez.
    
    Un callback es una función JavaScript que se ejecuta cuando una
    operación asíncrona ha terminado.

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 const http = require('http')
 const port = process.env.PORT || 8080
 
-const server = http.createServer()
+const server = http.createServer(function (req, res) {
+  res.end('Hello io.js')
+})
 
-server.listen(port)
+server.listen(port, function () {
+  console.log('Server running in port ' + port)
+})
