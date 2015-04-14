e03b42d6cbd0b1db11bb63250c86c7656ce55f5a
Julian Duque <julianduquej@gmail.com>
2015-04-14 15:32:48 -0500

    Agregar course para manejo de rutas
    En este commit utilizaremos el módulo `course` para manejar rutas del
    aplicación, este módulo funciona muy buen con el servidor `http` y nos
    da caracteristicas similares a `express`

--- router/index.js
+++ router/index.js
@@ -1,17 +1,24 @@
 const path = require('path')
+const course = require('course')
 const st = require('st')
 
+const router = course()
 const mount = st({
   path: path.join(__dirname, '..', 'public'),
-  index: 'index.html'
+  index: 'index.html',
+  passthrough: true
 })
 
 function onRequest (req, res) {
   mount(req, res, function (err) {
     if (err) return fail(err, res)
 
-    res.statusCode = 404
-    res.end(`404 Not Found: ${req.url}`)
+    router(req, res, function (err) {
+      if (err) return fail(err, res)
+
+      res.statusCode = 404
+      res.end(`404 Not Found: ${req.url}`)
+    })
   })
 }
 
