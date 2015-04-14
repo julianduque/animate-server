1499ea8085845db8c43b19fe5ccd1619921005ed
Julian Duque <julianduquej@gmail.com>
2015-04-14 10:24:33 -0500

    Refactor: Extraer callback a funciones
    Una muy buena práctica para evitar el temido `callback hell` es utilizar
    named functions, esto es, declarar las funciones que servirán como tus
    callbacks y utilizarlas en vez de crear funciones anónimas para cada
    llamado asíncrono.
    
    De esta manera es incluso mas fácil hacer mantenimiento de tu código.

--- server.js
+++ server.js
@@ -3,10 +3,14 @@
 const http = require('http')
 const port = process.env.PORT || 8080
 
-const server = http.createServer(function (req, res) {
-  res.end('Hello io.js')
-})
+const server = http.createServer(onRequest)
 
-server.listen(port, function () {
+server.listen(port, onListening)
+
+function onRequest (req, res) {
+  res.end('Hello io.js')
+}
+
+function onListening () {
   console.log('Server running in port ' + port)
-})
+}
