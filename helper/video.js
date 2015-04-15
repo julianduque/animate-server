623e5d23223f55573fbd2561620e0a919b86b6f2
Julian Duque <julianduquej@gmail.com>
2015-04-15 12:45:49 -0500

    Decodificar imagenes y guardarlas en disco
    El primer paso para convertir las imagenes a video es realizar la
    decodificación y guardar la imágen en disco duro, para eso utilizamos el
    módulo `data-uri-to-buffer` que me convierte de un `data-uri` a un
    binario (buffer), este buffer lo almacenamos en nuestro sistema de
    archivos utilizando streams del módulo `fs`.
    
    El directorio donde lo guardaremos es el temporal del sistema y para
    evitar conflictos con otros usuarios convirtiendo imágenes utilizaremos
    el módulo `uuid` para generar un nombre único.
    
    Ya que las imagenes son un arreglo vamos a utiizar la función
    `async.eachSeries` para aplicar la decodificación a cada una de las
    imágenes en orden.

--- helper/video.js
+++ helper/video.js
@@ -1,10 +1,18 @@
 'use strict'
 
+const os = require('os')
+const fs = require('fs')
+const path = require('path')
+const uuid = require('uuid')
 const async = require('async')
+const dataURIBuffer = require('data-uri-to-buffer')
 const EventEmitter = require('events').EventEmitter
 
 module.exports = function (images) {
   let events = new EventEmitter()
+  let count = 0
+  let baseName = uuid.v4()
+  let tmpDir = os.tmpDir()
 
   async.series([
     decodeImages,
@@ -15,7 +23,19 @@ module.exports = function (images) {
 
   // Decode images to files
   function decodeImages (done) {
-    done()
+    async.eachSeries(images, decodeImage, done)
+  }
+
+  // Decode a single image
+  function decodeImage (image, done) {
+    let fileName = `${baseName}-${count++}.jpg`
+    let buffer = dataURIBuffer(image)
+    let ws = fs.createWriteStream(path.join(tmpDir, fileName))
+
+    ws.on('error', done)
+      .end(buffer, done)
+
+    events.emit('log', `Converting ${fileName}`)
   }
 
   // Create video from images with ffmpeg
--- router/index.js
+++ router/index.js
@@ -20,6 +20,10 @@ router.post('/process', function (req, res) {
     if (Array.isArray(body.images)) {
       let converter = helper.convertVideo(body.images)
 
+      converter.on('log', function (msg) {
+        console.log(msg)
+      })
+
       converter.on('video', function (video) {
         res.setHeader('Content-Type', 'application/json')
         res.end(JSON.stringify({ video: video }))
