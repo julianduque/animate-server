eaed1df7d89370888bbbf0a0851fc925074aa05d
Julian Duque <julianduquej@gmail.com>
2015-04-15 15:22:37 -0500

    Codificar video en data-uri
    En este commit codificamos el video a data-uri para transmitirlo a la
    web, para esto volvemos a utilizar streams para leer el video que ya
    hemos creado con `ffmpeg` pero esta vez el pipe lo hacemos a un módulo
    llamado `concat-stream`, este módulo concatena todos los `chunks` del
    archivo en un buffer y luego este buffer lo convertimos a data-uril
    utilizando el método `toString` codificado a `base64`
    
    Luego en la función `convertFinished` emitimos el evento `video` con el
    video codificado.

--- helper/video.js
+++ helper/video.js
@@ -7,6 +7,7 @@ const uuid = require('uuid')
 const async = require('async')
 const dataURIBuffer = require('data-uri-to-buffer')
 const EventEmitter = require('events').EventEmitter
+const concat = require('concat-stream')
 const listFiles = require('./list')
 const ffmpeg = require('./ffmpeg')
 
@@ -15,6 +16,7 @@ module.exports = function (images) {
   let count = 0
   let baseName = uuid.v4()
   let tmpDir = os.tmpDir()
+  let video
 
   async.series([
     decodeImages,
@@ -51,7 +53,17 @@ module.exports = function (images) {
 
   // Encode video
   function encodeVideo (done) {
-    done()
+    let fileName = `${baseName}.webm`
+    let rs = fs.createReadStream(path.join(tmpDir, fileName))
+
+    events.emit('log', `Encoding video ${fileName}`)
+
+    rs.pipe(concat(function (videoBuffer) {
+      video = `data:video/webm;base64,${videoBuffer.toString('base64')}`
+      done()
+    }))
+
+    rs.on('error', done)
   }
 
   // Cleanup temp folder
@@ -84,9 +96,9 @@ module.exports = function (images) {
 
   // Convertion finished
   function convertFinished (err) {
-    setTimeout(function () {
-      events.emit('video', 'this will be the encoded video')
-    }, 500)
+    if (err) return events.emit('error', err)
+
+    events.emit('video', video)
   }
 
   return events
