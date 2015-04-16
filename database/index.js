8c28779c1d05c9c788e7184e9b9b40ba91b5d17b
Julian Duque <julianduquej@gmail.com>
2015-04-16 14:52:23 -0500

    Agregar ttl para controlar duración de los mensajes
    En este paso usamos el módulo `level-ttl` para definir un tiempo de vida
    de los registros en la base de datos, solo almacenaremos los registros
    por 10 minutos y luego los eliminaremos, para esta aplicación no nos
    interesa guarda toda la historia del chat, es algo temporal.

--- database/index.js
+++ database/index.js
@@ -1,17 +1,21 @@
 'use strict'
 
 const level = require('level')
+const ttl = require('level-ttl')
 const uuid = require('uuid')
 
 module.exports = function (options) {
   options = options || {}
 
-  const db = level('./messages.db')
+  let duration = options.duration || 10 * 60 * 1000
+
+  const db = ttl(level('./messages.db'), { checkFrequency: 10000 })
 
   function save (message, callback) {
     let key = `message-${Date.now()}-${uuid.v4()}`
     let options = {
-      valueEncoding: 'json'
+      valueEncoding: 'json',
+      ttl: duration
     }
 
     db.put(key, message, options, callback)
