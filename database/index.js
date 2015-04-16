a5a12f5793147b128b571fbed439b8d3deabd769
Julian Duque <julianduquej@gmail.com>
2015-04-16 14:47:09 -0500

    Guardamos los mensajes en la base de datos
    En este paso utilizaremos LevelDB para almacenar los mensajes en una
    base de datos NoSQL tipo key - value, estos mensajes tendran una vida
    util la cual configuraremos en los siguientes pasos.

--- database/index.js
+++ database/index.js
@@ -1,10 +1,20 @@
 'use strict'
 
+const level = require('level')
+const uuid = require('uuid')
+
 module.exports = function (options) {
   options = options || {}
 
+  const db = level('./messages.db')
+
   function save (message, callback) {
-    callback()
+    let key = `message-${Date.now()}-${uuid.v4()}`
+    let options = {
+      valueEncoding: 'json'
+    }
+
+    db.put(key, message, options, callback)
   }
 
   function list (callback) {
